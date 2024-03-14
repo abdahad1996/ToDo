6a0752b7d287ab52d7306ac27fc38ff5395917b1
abdul ahad <abdul_ahad1996@live.com>
2024-03-14 16:00:17 +0100

    - created todoShared framework

---
+++ RecursiveToDo/TodoShared/TodoShared.h
@@ -0,0 +1,18 @@
+//
+//  TodoShared.h
+//  TodoShared
+//
+//  Created by macbook abdul on 14/03/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TodoShared.
+FOUNDATION_EXPORT double TodoSharedVersionNumber;
+
+//! Project version string for TodoShared.
+FOUNDATION_EXPORT const unsigned char TodoSharedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TodoShared/PublicHeader.h>
+
+
